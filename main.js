5d19279dcc8a4915d9fda0aa588d8d6d89c91fae
Jody Christian <jchristian@afa.net>
2022-10-26 21:04:52 -0500

    added files

---
+++ main.js
@@ -0,0 +1,18 @@
+//imports go here for cleanliness
+import "./scss/style.scss"
+import { createClient } from "@supabase/supabase-js"
+//initialize Supabase
+const sbKey = ""
+const supabase = createClient("https://xyzcompany.supabase.co", sbKey)
+//main functions
+const fetchRecords = () => {
+    //do stuff
+}
+
+const updateRecord = () => {
+    //do stuff
+}
+
+const deleteRecord = () => {
+    ///do stuff
+}
